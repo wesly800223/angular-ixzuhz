24c958ded7b80d0ecab65d05f4cab7b854890eb8
wesly800223 <wesly800223@gmail.com>
2020-11-17 17:00:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ixzuhz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ixzuhz)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+
+import { CartService } from "../cart.service";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
+})
+export class CartComponent implements OnInit {
+  public items: any;
+  public checkoutForm: any;
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
+  }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+}
